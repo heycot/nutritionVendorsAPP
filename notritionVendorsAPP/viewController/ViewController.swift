01db4a69b2213654a85a3288638330b5a3993634
heycot <nguyentu15061996@gmail.com>
2019-02-25 13:31:43 +0700

    Design signin and signup screen

---
+++ notritionVendorsAPP/viewController/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  notritionVendorsAPP
+//
+//  Created by Tu (Callie) T. NGUYEN on 2/25/19.
+//  Copyright © 2019 Tu (Callie) T. NGUYEN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
