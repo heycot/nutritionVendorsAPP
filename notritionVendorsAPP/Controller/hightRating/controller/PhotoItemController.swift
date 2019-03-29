fa29383752eaa40c206b021dedf0cabb8a54bfec
heycot <nguyentu15061996@gmail.com>
2019-03-29 17:24:56 +0700

    chaneg number of section

--- notritionVendorsAPP/Controller/hightRating/controller/PhotoItemController.swift
+++ notritionVendorsAPP/Controller/hightRating/controller/PhotoItemController.swift
@@ -13,6 +13,7 @@ class PhotoItemController: UIViewController {
     @IBOutlet weak var collectionView: UICollectionView!
     
     var documents = [DocumentResponse]()
+    var imgArr: [CustomImageView] = [CustomImageView]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,9 +24,39 @@ class PhotoItemController: UIViewController {
         collectionView.contentInset = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
         
     }
+    
+    func setupData() {
+        for item in documents {
+            let image = CustomImageView()
+            image.loadImageUsingUrlString(urlString: BASE_URL_IMAGE + item.link! + ".jpg")
+            imgArr.append(image)
+        }
+    }
+    
+    
+    
+    func showImage(index: Int) {
+        let newImageView = imgArr[index]
+//        let newImageView = CustomImageView(image: imageView)
+        newImageView.frame = UIScreen.main.bounds
+        newImageView.backgroundColor = .black
+        newImageView.contentMode = .scaleAspectFit
+        newImageView.isUserInteractionEnabled = true
+        let tap = UITapGestureRecognizer(target: self, action: #selector(dismisHandle))
+        newImageView.addGestureRecognizer(tap)
+        self.view.addSubview(newImageView)
+        self.navigationController?.isNavigationBarHidden = true
+        self.tabBarController?.tabBar.isHidden = true
+    }
+    
+    @objc func dismisHandle() {
+        dismiss(animated: true, completion: nil)
+    }
 }
 
 extension PhotoItemController: UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return documents.count
@@ -37,6 +68,10 @@ extension PhotoItemController: UICollectionViewDelegate, UICollectionViewDataSou
         cell.updateView(image: documents[indexPath.row].link!)
         
         return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        showImage(index: indexPath.row)
     }
 }
 
@@ -52,6 +87,10 @@ extension PhotoItemController: UICollectionViewDelegateFlowLayout {
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
         return CGFloat(10.0)
+    }
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 3
     }
 }
 
