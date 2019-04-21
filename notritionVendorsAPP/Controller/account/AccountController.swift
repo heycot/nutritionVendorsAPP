3dbfd958af81279b062cdfb51f536f5cc56cbb9b
heycot <nguyentu15061996@gmail.com>
2019-04-21 18:41:13 +0700

    show accountproperty

--- notritionVendorsAPP/Controller/account/AccountController.swift
+++ notritionVendorsAPP/Controller/account/AccountController.swift
@@ -24,14 +24,38 @@ class AccountController: UIViewController {
     
     var listProperty = ["Login", "Payment", "History", "My Promo Code", "For Shop Owner", "Invite Friends", "Feedback", "User Policy", "App Settings", "Log Out"]
     
+    var index = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupView()
+        setupProperty()
+    }
+    
+    func setupView() {
+        navigationController?.navigationBar.barTintColor = APP_COLOR
         
         tableView.delegate = self
         tableView.dataSource = self
         
-        setupProperty()
-
+        tableView.tableFooterView = addVersion()
+        
+        self.tableView.reloadData()
+        tableView.estimatedRowHeight = UITableView.automaticDimension
+        tableView.rowHeight = 50
+    }
+    
+    func addVersion() -> UILabel{
+        let label = UILabel()
+        
+        label.frame = CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 100)
+        label.backgroundColor = .white
+        label.textAlignment = NSTextAlignment.center
+        label.text = "Version 1.0"
+        label.isHidden = false
+        label.numberOfLines = 0;
+        
+        return label
     }
     
     func setupProperty() {
@@ -43,7 +67,9 @@ class AccountController: UIViewController {
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.destination is AccountDetailController {
-            let vc = segue.destination as? AccountDetailController
+//            let vc = segue.destination as? AccountDetailController
+            
+        } else if segue.destination is LoginController {
             
         }
     }
@@ -53,25 +79,53 @@ class AccountController: UIViewController {
 extension AccountController : UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
-        performSegue(withIdentifier: , sender: nil)
+        
+        if indexPath.section == 0 {
+            if AuthServices.instance.isLoggedIn {
+                performSegue(withIdentifier: SegueIdentifier.accountToDetail.rawValue, sender: nil)
+            } else {
+                
+                performSegue(withIdentifier: SegueIdentifier.accountToLogin.rawValue, sender: nil)
+            }
+        }
     }
 }
 
 extension AccountController : UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 4
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return listProperty.count
+        switch section {
+        case 0:
+            return 1
+        case 1:
+            return 3
+        case 2:
+            return 2
+        default:
+            return 3
+        }
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: CellIdentifier.accountCell.rawValue, for: indexPath) as! AccountCell
         
-        cell.updateView(iconName: listAccountProperty[indexPath.row].icon, property_name: listAccountProperty[indexPath.row].property, property_action: listAccountProperty[indexPath.row].action)
+        
+        cell.updateView(iconName: listAccountProperty[index].icon, property_name: listAccountProperty[index].property, property_action: listAccountProperty[index].action)
+        
+        index = index + 1
         return cell
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(true)
         tableView.reloadData()
+    }
+    
+    func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        return 15.0
     }
     
 }
