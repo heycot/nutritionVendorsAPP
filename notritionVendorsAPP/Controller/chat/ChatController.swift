69b7b3f87533c4e434c040dc2795474f9727c26a
heycot <nguyentu15061996@gmail.com>
2019-05-22 13:50:25 +0700

    remove account controller

---
+++ notritionVendorsAPP/Controller/chat/ChatController.swift
@@ -0,0 +1,40 @@
+//
+//  ChatController.swift
+//  notritionVendorsAPP
+//
+//  Created by Tu (Callie) T. NGUYEN on 5/22/19.
+//  Copyright © 2019 Tu (Callie) T. NGUYEN. All rights reserved.
+//
+
+import UIKit
+
+class ChatController: UIViewController {
+
+//    let uiConfig = ATCChatUIConfiguration(primaryColor: UIColor(hexString: "#0084ff"),
+//                                          secondaryColor: UIColor(hexString: "#f0f0f0"),
+//                                          inputTextViewBgColor: UIColor(hexString: "#f4f4f6"),
+//                                          inputTextViewTextColor: .black,
+//                                          inputPlaceholderTextColor: UIColor(hexString: "#979797"))
+//    let channel = ATCChatChannel(id: "channel_id", name: "Chat Title")
+//    let viewer = ATCUser(firstName: "Florian", lastName: "Marcu")
+//    let chatVC = ATCChatThreadViewController(user: viewer, channel: channel, uiConfig: uiConfig)
+//
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//
+//        // Do any additional setup after loading the view.
+//        self = chatVC
+//    }
+//
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
