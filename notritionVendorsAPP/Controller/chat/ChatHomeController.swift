c5e4b9c4c5814aba425b3da72b87777da82af4a1
heycot <nguyentu15061996@gmail.com>
2019-05-24 17:09:33 +0700

    add ChatHomeController

---
+++ notritionVendorsAPP/Controller/chat/ChatHomeController.swift
@@ -0,0 +1,19 @@
+//
+//  ChatHomeController.swift
+//  notritionVendorsAPP
+//
+//  Created by Tu (Callie) T. NGUYEN on 5/24/19.
+//  Copyright © 2019 Tu (Callie) T. NGUYEN. All rights reserved.
+//
+
+import UIKit
+
+class ChatHomeController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+
+
+}
