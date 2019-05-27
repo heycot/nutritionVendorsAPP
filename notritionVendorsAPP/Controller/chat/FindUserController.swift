c29ed1161d651d2b7646b06ceb407e47fee16f53
heycot <nguyentu15061996@gmail.com>
2019-05-27 09:53:49 +0700

    handle list user in FindUserController

--- notritionVendorsAPP/Controller/chat/FindUserController.swift
+++ notritionVendorsAPP/Controller/chat/FindUserController.swift
@@ -7,17 +7,100 @@
 //
 
 import UIKit
+import PKHUD
 
 class FindUserController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var notification: UILabel!
     
+    let searchBar = UISearchBar()
+    var listUser = [UserResponse]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        navigationController?.navigationBar.barTintColor = APP_COLOR
+        notification.isHidden = true
+        createSearchBar()
+        setupTableView()
     }
     
+    func createSearchBar() {
+        searchBar.showsCancelButton = false
+        searchBar.placeholder = " Search here"
+        searchBar.delegate = self
+        searchBar.tintColor = .black
+        
+        self.navigationItem.titleView = searchBar
+        searchBar.becomeFirstResponder()
+    }
+    
+    func setupTableView() {
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.tableFooterView = UIView()
+        
+        self.tableView.reloadData()
+        tableView.estimatedRowHeight = UITableView.automaticDimension
+        tableView.rowHeight = 80
+    }
+    
+    @objc func search() {
+        HUD.show(.progress)
+        
+//        SearchServices.instance.search(searchText: searchBar.text!.lowercased()) { (data) in
+//            guard let data = data else { return }
+//            self.listUser = data
+//
+//            HUD.hide()
+//            self.tableView.reloadData()
+//        }
+    }
+
+}
 
 
+extension FindUserController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return listUser.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: CellIdentifier.userCell.rawValue ) as! UserCell
+        
+        cell.updateView(user: listUser[indexPath.row])
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: false)
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        tableView.reloadData()
+    }
+    
+}
+
+
+// handle UISearchBar
+extension FindUserController: UISearchBarDelegate {
+    func scrollViewWillBeginDragging(_ scrollView: UIScrollView) {
+        searchBar.endEditing(true)
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        search()
+        
+        searchBar.endEditing(true)
+    }
+    
+    
+    func searchBar(_ searchBar: UISearchBar, selectedScopeButtonIndexDidChange selectedScope: Int) {
+        
+    }
 }
